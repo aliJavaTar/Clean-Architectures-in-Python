fc3c462e420b3aa70dff6736851538f54006f219
ali <ali77erfagh@gmail.com>
2024-02-01 12:01:36 +0330

    add packagin and add User and Article class (entity) add id -> value object

---
+++ src/domain/entity/Article.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from src.domain import Id
+
+
+class Article:
+    article_id: Id
+    title: str
+    content: str
+    brief: str
+    is_published: bool
+    created_at: datetime
+    updated_at: datetime
+
+    def __init__(self, article_id: int, title: str, content: str, brief: str, is_published: bool):
+        self.article_id = article_id
+        self.title = title
+        self.content = content
+        self.brief = brief
+        self.is_published = is_published
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
---
+++ src/domain/entity/User.py
@@ -0,0 +1,16 @@
+from src.domain import Id
+from src.domain.entity import Article
+
+
+class User:
+    user_id: Id
+    username: str
+    password: str
+    national_code: int
+    article: Article
+
+    def __init__(self, user_id: Id, username: str, password: str, national_code: int):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.national_code = national_code
